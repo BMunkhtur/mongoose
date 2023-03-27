f756e615279e99271bd223c77d824d023287c649
BMunkhtur <bayarsaikhanmunkhtur@gmail.com>
2023-03-27 12:52:05 +0800

    up

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,19 +2,6 @@ const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const getAllUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    if (!users) {
-      res.status(200).json({ message: "Хэрэглэгчдийн мэдээлэл хоосон байна." });
-    }
-
-    res.status(201).json({ message: "Хэрэглэгчдийн мэдээлэл олдлоо.", users });
-  } catch (error) {
-    next(error);
-  }
-};
-
 const createUser = async (req, res, next) => {
   const { name, email, profileImg } = req.body;
   const salt = await bcrypt.genSalt(10);
